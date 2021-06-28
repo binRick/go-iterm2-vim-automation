737c77e22641312bbb8d596267d865bdd92c5cf6
Your Name <you@example.com>
2021-06-28 13:50:39 -0400

    improve werapper script

--- local_server/gen_seq.sh
+++ local_server/gen_seq.sh
@@ -2,6 +2,8 @@
 set -e
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+export NOTIFY_INTERVAL=${NOTIFY_INTERVAL:-5}
+
 CONTROL_SEQUENCE_NAME=${CONTROL_SEQUENCE_NAME:-vim-iterm}
 CONTROL_SEQUENCE_PREFIX="${1:-test-seq}"
 CONTROL_SEQUENCE_DATA="${2:--}"
@@ -15,7 +17,7 @@ fi
 
 cwd="$(pwd)"
 
-jo_cmd="jo dat='$(echo -e "$DAT"|jq -Mrc)' ts=$(date +%s) hostname='$(command hostname -f)' user=$(id -nu) cwd='$cwd' ITERM_SESSION_ID='$ITERM_SESSION_ID' ITERM_PROFILE='$ITERM_PROFILE' pid=$$"
+jo_cmd="jo dat='$(echo -e "$DAT"|jq -Mrc)' ts=$(date +%s) hostname='$(command hostname -f)' user=$(id -nu) cwd='$cwd' ITERM_SESSION_ID='$ITERM_SESSION_ID' ITERM_PROFILE='$ITERM_PROFILE' pid=$$ SSH_CONNECTION='$SSH_CONNECTION'"
 encode_cmd="base64 -w0"
 DAT="$(eval $jo_cmd | jq -Mrc | $encode_cmd)"
 
