bda432cc76eb3723c50a79682aa5f5a32330135c
Your Name <you@example.com>
2021-06-28 14:21:10 -0400

    m

--- local_server/gen_seq.sh
+++ local_server/gen_seq.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 export NOTIFY_INTERVAL=${NOTIFY_INTERVAL:-5}
 
@@ -19,7 +20,7 @@ cwd="$(pwd)"
 
 jo_cmd="jo dat='$(echo -e "$DAT"|jq -Mrc)' ts=$(date +%s) hostname='$(command hostname -f)' user=$(id -nu) cwd='$cwd' ITERM_SESSION_ID='$ITERM_SESSION_ID' ITERM_PROFILE='$ITERM_PROFILE' pid=$$ SSH_CONNECTION='$SSH_CONNECTION'"
 encode_cmd="base64 -w0"
-DAT="$(eval $jo_cmd | jq -Mrc | $encode_cmd)"
+DAT="$(eval $jo_cmd | command jq -Mrc | $encode_cmd)"
 
 msg="\033]1337;Custom=id=${CONTROL_SEQUENCE_NAME}:${CONTROL_SEQUENCE_PREFIX}:${DAT}\a"
 echo -e "$msg"
